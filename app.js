550c4d43eada6ee82b983950403477852fd1e440
SR <saodat.riskiyeva@gmail.com>
2025-09-02 16:08:21 -0400

    working at request and response

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require("http");
 
 function handleRequest(request, response) {
-  response.statusCode = 200;
-  response.end("<h1>Hello World!</h1>");
+  // http://localhost:3000/
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<h1>" + new Date().toISOString() + "</h1>");
+  } else if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("<h1>Hello World!</h1>");
+  }
 }
 
 const server = http.createServer(handleRequest);
