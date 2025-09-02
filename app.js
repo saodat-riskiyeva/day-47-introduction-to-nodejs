22500e0bee83a006e3a8e3712d302a7162a7b2d5
SR <saodat.riskiyeva@gmail.com>
2025-09-02 15:59:31 -0400

    creating a custom response

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require("http");
 
-const server = http.createServer();
+function handleRequest(request, response) {
+  response.statusCode = 200;
+  response.end("<h1>Hello World!</h1>");
+}
+
+const server = http.createServer(handleRequest);
 
 server.listen(3000);
 
