ca789e3e5d73918fba8d4526eb2522f306746959
SR <saodat.riskiyeva@gmail.com>
2025-09-02 16:31:50 -0400

    re-qriting code with express

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const http = require("http");
+const express = require("express");
 
-function handleRequest(request, response) {
-  // http://localhost:3000/
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1>");
-  } else if (request.url === "/") {
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  }
-}
+const app = express();
 
-const server = http.createServer(handleRequest);
+// http://localhost:3000/currenttime
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
 
-server.listen(3000);
+// http://localhost:3000/
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(3000);
+
+// function handleRequest(request, response) {
+//   // http://localhost:3000/
+//   if (request.url === "/currenttime") {
+//     response.statusCode = 200;
+//     response.end("<h1>" + new Date().toISOString() + "</h1>");
+//   } else if (request.url === "/") {
+//     response.statusCode = 200;
+//     response.end("<h1>Hello World!</h1>");
+//   }
+// }
+
+// const server = http.createServer(handleRequest);
+
+// server.listen(3000);
 
 // amazon.com
 // amazon.com:80
